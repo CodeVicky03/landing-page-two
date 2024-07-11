d082d23dfb2e06fb8c520aba4d82e15df74f5318
Vicky <lazyboy3300@gmail.com>
2024-07-11 18:08:58 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,52 @@ import Last from "./Last";
 import Selfie from "./Selfie";
 import Pic from "./Pic";
 import Footer from "./Footer";
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 function App() {
+  const [LoadingPage, setLoadingPage] = useState(true);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setLoadingPage(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <section className="home-page">
-        <div className="landing-page">
-          <video muted autoPlay loop>
-            <source src={xiaomi} type="video/mp4"></source>
-          </video>
+    <>
+      {LoadingPage ? (
+        <Loading />
+      ) : (
+        <div>
+          <section className="home-page">
+            <div className="landing-page">
+              <video muted autoPlay loop>
+                <source src={xiaomi} type="video/mp4"></source>
+              </video>
+            </div>
+            <Navbar />
+            <div className="content">
+              <p>$1120</p>
+              <h1>XIAOMI 14 ULTRA</h1>
+              <button style={{ backgroundColor: "#ff6900" }}>BUY NOW</button>
+              <button style={{ backgroundColor: "#313131" }}>SEE MORE</button>
+            </div>
+            <div className="collection">
+              <img src={one} alt="" draggable="false" />
+              <img src={two} alt="" draggable="false" />
+            </div>
+          </section>
+          <Show />
+          <Last />
+          <Selfie />
+          <Pic />
+          <Footer />
         </div>
-        <Navbar />
-        <div className="content">
-          <p>$1120</p>
-          <h1>XIAOMI 14 ULTRA</h1>
-          <button style={{ backgroundColor: "#ff6900" }}>BUY NOW</button>
-          <button style={{ backgroundColor: "#313131" }}>SEE MORE</button>
-        </div>
-        <div className="collection">
-          <img src={one} alt="" draggable="false" />
-          <img src={two} alt="" draggable="false" />
-        </div>
-      </section>
-      <Show />
-      <Last/>
-      <Selfie/>
-      <Pic/>
-      <Footer/>
-    </div>
+      )}
+    </>
   );
 }
 
---
+++ src/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="lp">
+        <div className="load"></div>
+        <p>LANDING PAGE LOADING</p>
+    </div>
+  )
+}
+
+export default Loading
